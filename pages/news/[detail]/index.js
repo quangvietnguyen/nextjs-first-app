e72eaf65986e979a7830c46d369ccc5db6fb9b09
nguye374 <nguye374@sheridancollege.ca>
2021-10-13 00:44:26 -0400

    Link update

--- pages/news/[detail]/index.js
+++ pages/news/[detail]/index.js
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router';
 // our-domain.com/news/detail
 
 function DetailPage() {
+    const router = useRouter();
+
+    console.log(router.query.detail);
     return <h1>Detail Page</h1>
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
 // our-domain.com/news/
+import Link from 'next/link';
+import { Fragment } from "react";
 
 function NewsPage() {
-    return <h1>News Page</h1>
+    return (
+        <Fragment>
+            <h1>News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/next1">
+                        Next 1
+                    </Link>
+                </li>
+                <li>Next 2</li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default NewsPage;
